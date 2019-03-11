80487f27c9159f7f28a11b0924f250161dca936d
Lwq <943904454@qq.com>
2019-03-12 01:02:00 +0800

    完成部门更新接口开发

--- src/main/java/com/lwq/service/SysDeptService.java
+++ src/main/java/com/lwq/service/SysDeptService.java
@@ -1,15 +1,20 @@
 package com.lwq.service;
 
+import com.google.common.base.Preconditions;
 import com.lwq.dao.SysDeptMapper;
 import com.lwq.exception.ParamException;
 import com.lwq.model.SysDept;
 import com.lwq.param.DeptParam;
 import com.lwq.util.BeanValidator;
 import com.lwq.util.LevelUtil;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @Author: Lwq
@@ -29,8 +34,8 @@ public class SysDeptService {
             throw new ParamException("同一层级下存在相同名称的部门");
         }
         SysDept dept = SysDept.builder()
-                .id(param.getId())
                 .name(param.getName())
+                .parentId(param.getParentId())
                 .seq(param.getSeq())
                 .remark(param.getRemark()).build();
 
@@ -42,9 +47,53 @@ public class SysDeptService {
         sysDeptMapper.insertSelective(dept);
     }
 
+    public void update(DeptParam param){
+        BeanValidator.check(param);
+        SysDept beforeDept = sysDeptMapper.selectByPrimaryKey(param.getId());
+        Preconditions.checkNotNull(beforeDept,"待更新的部门不存在");
+        if(checkExist(param.getParentId(),param.getName(),param.getId())){
+            throw new ParamException("同一层级下存在相同名称的部门");
+        }
+
+        SysDept afterDept = SysDept.builder()
+                .id(param.getId())
+                .name(param.getName())
+                .parentId(param.getParentId())
+                .seq(param.getSeq())
+                .remark(param.getRemark()).build();
+
+        afterDept.setLevel(LevelUtil.calculateLevel(getLevel(param.getParentId()),param.getParentId()));
+        afterDept.setOperator("system-update");//TODO
+        afterDept.setOperateIp("127.0.0.1");
+        afterDept.setOperateTime(new Date());
+
+        updateWithChild(beforeDept,afterDept);
+    }
+
+    @Transactional
+    public void updateWithChild(SysDept before, SysDept after){
+
+        String newLevelPerfix = after.getLevel();
+        String oldLevelPerfix = before.getLevel();
+
+        if(StringUtils.equals(oldLevelPerfix,newLevelPerfix)){
+            List<SysDept> deptList = sysDeptMapper.getChildDeptListByLevel(oldLevelPerfix);
+            if(CollectionUtils.isNotEmpty(deptList)){
+                for(SysDept dept:deptList){
+                    String level = dept.getLevel();
+                    if(level.indexOf(oldLevelPerfix)==0){
+                        level = newLevelPerfix+level.substring(oldLevelPerfix.length());
+                        dept.setLevel(level);
+                    }
+                }
+                sysDeptMapper.batchUpdateLevel(deptList);
+            }
+        }
+        sysDeptMapper.updateByPrimaryKey(after);
+    }
+
     private boolean checkExist(Integer parentId,String deptName,Integer deptId){
-//        TODO
-        return true;
+        return sysDeptMapper.countByNameAndParentId(parentId,deptName,deptId) > 0;
     }
 
     private String getLevel(Integer deptId){
@@ -54,4 +103,6 @@ public class SysDeptService {
         }
         return dept.getLevel();
     }
+
+
 }
